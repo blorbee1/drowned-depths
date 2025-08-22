33c689627a68ccf585268735e63f09e6fed5693c
blorbee1 <ztn10work@gmail.com>
2025-08-22 16:05:23 -0400

    Initial commit

---
+++ src/main/java/com/blorbee/drowneddepths/DrownedDepthsClient.java
@@ -0,0 +1,10 @@
+package com.blorbee.drowneddepths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrownedDepthsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/blorbee/drowneddepths/DrownedDepthsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.blorbee.drowneddepths;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DrownedDepthsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
